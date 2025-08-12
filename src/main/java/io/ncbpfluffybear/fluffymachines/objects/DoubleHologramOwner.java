a1232fdbc981dadb1e59eb71706362ce45694d76
LobbyTech <1187755698@qq.com>
2025-08-12 21:09:29 +0800

    1

--- src/main/java/io/ncbpfluffybear/fluffymachines/objects/DoubleHologramOwner.java
+++ src/main/java/io/ncbpfluffybear/fluffymachines/objects/DoubleHologramOwner.java
@@ -14,13 +14,6 @@ import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.implementation.items.blocks.HologramProjector;
 import io.ncbpfluffybear.fluffymachines.utils.Utils;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-import org.bukkit.Location;
-import org.bukkit.block.Block;
-import org.bukkit.util.Vector;
-
 /**
  * This {@link ItemAttribute} manages holograms.
  * Modified version of {@link io.github.thebusybiscuit.slimefun4.core.attributes.HologramOwner}
